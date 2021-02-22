2508321d5e199300bee1e184dfaa9f754a57c5cd
Milena <milenaalmeidadev@hotmail.com>
2021-02-22 14:29:02 -0400

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Hello - NLW #04'
+    });
+});
+
+app.post('/', (request, response) => {
+    return response.json({
+        message: 'Os dados foram salvos com sucesso!'
+    });
+});
+
+app.listen(3333, () => {
+    console.log('Server is running!');
+});
